4ba518401374f309ff3f634cd8e594d5d0f5152b
Nitin Kumar <nitin.cypher@gmail.com>
2020-12-13 21:41:58 -0500

    CORS fixes

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import xkcd
 from flask import Flask
-app = Flask(__name__)
+from flask_cors import CORS
+import json
 
+app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/getRandomXkcdLink')
 def hello():
     random = xkcd.getRandomComic()
     response = { "imageLink": random.getImageLink(), "imageTitle": random.getTitle(), "altText": random.getAltText()}
-    return response
+    return json.dumps(response)
 
 if __name__ == '__main__':
     app.run()
